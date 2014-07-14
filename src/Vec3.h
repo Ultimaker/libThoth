27cc1eb49855b7d55b30ada5eee2aafbc9e78dd6
nallath <nallath@gmail.com>
2014-07-14 11:16:26 +0200

    Added vec3

---
+++ src/Vec3.h
@@ -0,0 +1,55 @@
+#ifndef LIB_THOTH_VEC3_H
+#define LIB_THOTH_VEC3_H
+#include <iostream>
+
+namespace lth
+{
+	template <typename T>
+	class Vec3
+	{
+	public:
+		Vec3 (T _x,T _y, T _z): x(_x), y(_y), z(_z) {}
+		Vec3 () {}
+		Vec3<T> operator + (const Vec3<T>& v) const { return Vec3<T>(x+v.x, y+v.y, z+v.z); }
+		Vec3<T> operator - (const Vec3<T>& v) const { return Vec3<T>(x-v.x, y-v.y, z-v.z); }
+		Vec3<T> operator / (const T i) const { return Vec3(x/i, y/i, z/i); }
+
+		Vec3<T>& operator += (const Vec3<T>& v) { x += v.x; y += v.y; z += v.z; return *this; }
+		Vec3<T>& operator -= (const Vec3<T>& v) { x -= v.x; y -= v.y; z -= v.z; return *this; }
+
+		bool operator == (const Vec3<T>& v) const { return x==v.x&&y==v.y&&z==v.z; }
+		bool operator != (const Vec3<T>& v) const { return x!=v.x||y!=v.y||z!=v.z; }
+
+
+		T max()
+        {
+            if (x > y && x > z) return x;
+            if (y > z) return y;
+            return z;
+        }
+
+        Vec3<T> cross(const Vec3<T>& v)
+        {
+            return Vec3<T>(
+            y*v.z-z*v.y,
+            z*v.x-x*v.z,
+            x*v.y-y*v.x);
+        }
+
+        T dot(const Vec3<T>& v)
+        {
+            return (v.x * x + v.y * y + v.z * z);
+        }
+
+		T x;
+		T y;
+		T z;
+	protected:
+
+	};
+
+
+}
+
+
+#endif //LIB_THOTH_VEC3_H
