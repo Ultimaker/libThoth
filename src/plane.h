341cde306887a5211282952afe4ce1c00255ea53
nallath <nallath@gmail.com>
2014-07-14 15:03:13 +0200

    Added missing template declarations

--- src/plane.h
+++ src/plane.h
@@ -14,26 +14,28 @@ namespace lth
 		Plane(T &_normal, U &_distance);
 		Plane();
 		//rotateByLine(const Line line, const U rotation);
-	
+
     	protected:
 		T normal;
 		U distance;
 	};
+	template <typename T, typename U>
 	Plane<T,U>::Plane(){}
+	template <typename T, typename U>
 	Plane<T,U>::Plane(T &_normal, U &_distance)
 	{
 		if(lth::checkValidTemplateArgument<T>())
 		{
 			normal = _normal;
 			distance = _distance;
-		} else 
+		} else
 		{
 			throw std::invalid_argument( "Plane was templated with unusable class.");
 		}
 	}
 
 	//void Plane<T,U>::rotateByLine(const Line line, const float rotation){}
-	
+
 }
 
 #endif //LIB_THOTH_PLANE_H
